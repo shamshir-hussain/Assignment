344a610679c8da8cb07f3c30d905a7336d97905b
shamshir-hussain <shamshir.hussain08@gmail.com>
2024-03-12 21:54:41 +0530

    Practice Problems

---
+++ js/assignment.js
@@ -0,0 +1,246 @@
+//1.Leap Year
+// let year = Number(prompt("Enter a year"));
+// switch (true) {
+//   case year % 4 == 0 && year % 100 !== 0:
+//     console.log(year + " is a leap year");
+//     break;
+//   case year % 400 == 0:
+//     console.log(year + "is a leap year");
+//     break;
+//   default:
+//     console.log(year + "not a leap year");
+// }
+
+//  2.Temperature
+// let temp = Number(prompt("Enter temparature"));
+
+// if (temp >= 30) {
+//   console.log("Hot");
+// } else if (temp >= 20 && temp < 30) {
+//   console.log("Neutral");
+// } else {
+//   console.log("cold");
+// }
+
+// 3.Vowel Checker
+
+// let value = prompt("Enter Alphabet");
+
+// value == "a" ||
+// value == "e " ||
+// value == "i" ||
+// value == "o" ||
+// value == "u" ||
+// value == "A" ||
+// value == "E" ||
+// value == "I" ||
+// value == "O" ||
+// value == "U"
+//   ? console.log("Vowel")
+//   : console.log("Not vowel");
+
+//4. odd or even
+
+// let num = Number(prompt("Enter the Number"));
+
+// num % 2 == 0 ? console.log(" even ") : console.log("Odd ");
+
+//5. checking of months
+// let month = prompt("Enter the month or number");
+
+// switch (month) {
+//   case "1":
+//     console.log("January");
+//     break;
+//   case "january":
+//     console.log("1");
+//     break;
+//   case "January":
+//     console.log("1");
+//     break;
+//   case "2":
+//     console.log("February");
+//     break;
+//   case "february":
+//     console.log("2");
+//     break;
+//   case "February":
+//     console.log("2");
+//     break;
+//   case "3":
+//     console.log("March");
+//     break;
+//   case "march":
+//     console.log("3");
+//     break;
+//   case "March":
+//     console.log("3");
+//     break;
+//   case "4":
+//     console.log("April");
+//     break;
+//   case "april":
+//     console.log("4");
+//     break;
+//   case "April":
+//     console.log("4");
+//     break;
+//   case "5":
+//     console.log("May");
+//     break;
+//   case "May":
+//     console.log("5");
+//     break;
+//   case "may":
+//     console.log("5");
+//     break;
+//   case "6":
+//     console.log("June");
+//     break;
+//   case "June":
+//     console.log("6");
+//     break;
+//   case "june":
+//     console.log("6");
+//     break;
+//   case "7":
+//     console.log("July");
+//     break;
+//   case "July":
+//     console.log("7");
+//     break;
+//   case "july":
+//     console.log("7");
+//     break;
+//   case "8":
+//     console.log("August");
+//     break;
+//   case "August":
+//     console.log("8");
+//     break;
+//   case "august":
+//     console.log("8");
+//     break;
+//   case "9":
+//     console.log("September");
+//     break;
+//   case "September":
+//     console.log("9");
+//     break;
+//   case "september":
+//     console.log("9");
+//     break;
+//   case "10":
+//     console.log("October");
+//     break;
+//   case "October":
+//     console.log("10");
+//     break;
+//   case "october":
+//     console.log("10");
+//     break;
+//   case "11":
+//     console.log("November");
+//     break;
+//   case "November":
+//     console.log("10");
+//     break;
+//   case "november":
+//     console.log("10");
+//     break;
+//   case "12":
+//     console.log("December");
+//     break;
+//   case "December":
+//     console.log("12");
+//     break;
+//   case "december":
+//     console.log("12");
+//     break;
+
+//   default:
+//     console.log("Invaild month");
+// }
+
+//6. Week days
+
+// let month = prompt("Enter the Day or Number");
+
+// switch (month) {
+//   case "1":
+//     console.log("Monday");
+//     break;
+//   case "Monday":
+//     console.log("1");
+//     break;
+//   case "monday":
+//     console.log("1");
+//     break;
+//   case "2":
+//     console.log("Tuesday");
+//     break;
+//   case "tuesday":
+//     console.log("2");
+//     break;
+//   case "Tuesday":
+//     console.log("2");
+//     break;
+//   case "3":
+//     console.log("Wednesday");
+//     break;
+//   case "Wednesday":
+//     console.log("3");
+//     break;
+//   case "wednesday":
+//     console.log("3");
+//     break;
+//   case "4":
+//     console.log("Thursday");
+//     break;
+//   case "thursday":
+//     console.log("4");
+//     break;
+//   case "Thursday":
+//     console.log("4");
+//     break;
+//   case "5":
+//     console.log("Friday");
+//     break;
+//   case "Friday":
+//     console.log("5");
+//     break;
+//   case "friday":
+//     console.log("5");
+//     break;
+//   case "6":
+//     console.log("Saturday");
+//     break;
+//   case "Saturday":
+//     console.log("6");
+//     break;
+//   case "saturday":
+//     console.log("6");
+//     break;
+//   case "7":
+//     console.log("Sunday");
+//     break;
+//   case "Sunday":
+//     console.log("7");
+//     break;
+//   case "sunday":
+//     console.log("7");
+//     break;
+//   default:
+//     console.log("Invalid Day");
+// }
+
+// 7.check weather the triangle
+
+// let x = Number(prompt("Enter Number"));
+// let y = Number(prompt("Enter Number"));
+// let z = Number(prompt("Enter Number"));
+// x === y && y === z && z === x
+//   ? console.log("Equilateral Triangel")
+//   : x === y || y === z || z === x
+//   ? console.log("Isoclene Triangle")
+//   : console.log("Scalene Traingle");
